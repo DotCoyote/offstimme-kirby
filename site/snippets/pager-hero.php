dba93fc75782ed4acf991c80559d9b2272536fcc
Lars Eichler <lars.eichler@turbinekreuzberg.com>
2022-09-28 08:22:22 +0200

    Add link titles

--- site/snippets/pager-hero.php
+++ site/snippets/pager-hero.php
@@ -6,13 +6,15 @@
              style="background-image: url('<?= $image->url() ?>')"></div>
     <?php endforeach; ?>
     <div class="bg-primary-600 inset-0 absolute z-10 bg-opacity-70 flex justify-center items-center align-middle px-8 ">
-        <a href="/" class="logo block [&>*]:w-[25rem] [&>*]:h-[5.125rem] [&>*]:max-w-full [&>*]:max-h-full text-white">
+        <a href="/" class="logo block [&>*]:w-[25rem] [&>*]:h-[5.125rem] [&>*]:max-w-full [&>*]:max-h-full text-white"
+           title="Offstimme Logo">
             <?= svg('src/assets/logo_horizontal.svg') ?>
         </a>
 
         <div class="absolute bottom-8 inset-x-0 text-center text-white">
             <a href="#main"
-               class="bg-gray-800 bg-opacity-50 w-16 h-16 js-scroll-to inline-flex items-center justify-center [&>*]:w-[1.5rem] [&>*]:h-[1.25rem]">
+               class="bg-gray-800 bg-opacity-50 w-16 h-16 js-scroll-to inline-flex items-center justify-center [&>*]:w-[1.5rem] [&>*]:h-[1.25rem]"
+               title="scroll down">
                 <?= svg('src/assets/icons/chevron_down.svg') ?>
             </a>
         </div>
